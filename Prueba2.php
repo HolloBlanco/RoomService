d971d0c5c2ffeabf174d1f93c4fa2ad280798388
Xammy <ymmax550@gmail.com>
2020-06-15 23:17:53 -0500

    Ultimo cambio y prueba de revert

--- Prueba2.php
+++ Prueba2.php
@@ -8,9 +8,5 @@
 	}
 
 	$aoe=$_POST["pelicula"];  
-<<<<<<< HEAD
-
-	echo "Esto es la prueba para saber el funcionamiento de 'git pull' "; 
-=======
->>>>>>> 737b6bfbc29ee49a7cfc6b7d301e70df639f1407
+	echo $aoe;
 ?>
