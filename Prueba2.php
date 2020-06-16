dc6e837cabffb0228a9236a6572ccb3d493b5f85
Xammy <ymmax550@gmail.com>
2020-06-15 23:14:54 -0500

    Probando 1 2 3

--- Prueba2.php
+++ Prueba2.php
@@ -10,11 +10,7 @@
 	$aoe=$_POST["pelicula"];  
 <<<<<<< HEAD
 
-<<<<<<< HEAD
 	echo "Esto es la prueba para saber el funcionamiento de 'git pull' "; 
 =======
 >>>>>>> 737b6bfbc29ee49a7cfc6b7d301e70df639f1407
-=======
-	echo "Esto es la prueba para saber el funcionamiento de 'git pull' ";
->>>>>>> parent of 0548fe5... Resolviendo erreres
 ?>
